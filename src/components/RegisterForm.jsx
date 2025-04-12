88c3a5f694e09fa39604013502c7c13ae8056164
ibraloyza <ibraahim3523@gmail.com>
2025-04-12 09:29:58 +0300

    add simple form with required fields

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function RegisterForm() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState:{ errors } } = useForm();
+    const onSubmit = (data) => console.log(data);
+    console.log(watch("example"))
+
+    // const {name, ref,onChange,onBlur} = Register("userName");
   return (
-    <div>
-        <form action="form">
-            <label htmlFor="">UserName</label>
-            <input type="text" name="user" id="user" />
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="pass">Password</label>
-            <input type="password" name="password" id="password" />
-            <button type='submit'>Register</button>
-        </form>
-    </div>
+   
+    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {/* register your input into the hook by invoking the "register" function */}
+      <input  {...register("example",{required:true})} />
+      {errors.example && <span>this is example and also is required</span>}
+
+      {/* include validation with required or other standard HTML validation rules */}
+      <input {...register("exampleRequired", { required: true })} />
+      {/* errors will return when field validation fails  */}
+      {errors.exampleRequired && <span>This field is required</span>}
+
+      <input type="submit" />
+    </form>
+  
   )
 }
 
